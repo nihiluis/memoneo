5cb39545b39c43d43faa0c6725d842d94e00795a
nihiluis <24858082+nihiluis@users.noreply.github.com>
2025-09-20 21:17:15 +0200

    fix: Read urls for gqlwrapper from env

--- backend/gqlwrapper/src/env.ts
+++ backend/gqlwrapper/src/env.ts
@@ -1,16 +1,7 @@
-import { createRequire } from "module"
-const require = createRequire(import.meta.url)
-
-const config = require("../config.json")
-
 export const DEV = process.env.NODE_ENV === "development"
 
-export const BASE_PATH = DEV ? config.basePath.dev : config.basePath.prod
-
-export const AUTH_API_URL = DEV ? config.authApiUrl.dev : config.authApiUrl.prod
-export const MASTER_API_URL = DEV
-  ? config.masterApiUrl.dev
-  : config.masterApiUrl.prod
+export const AUTH_API_URL = process.env.AUTH_API_URL ?? "http://localhost:8089"
+export const MASTER_API_URL = process.env.MASTER_API_URL ?? "http://localhost:8084"
 
 export const ENDPOINT_AUTH_URL = `${AUTH_API_URL}/auth`
 export const ENDPOINT_GQL_URL = `${MASTER_API_URL}/v1/graphql`
